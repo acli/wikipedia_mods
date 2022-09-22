91587b96ef13fb4f05c480fc805e9ec4a3478a67
Ambrose Li <ambrose.li@gmail.com>
2022-09-22 18:33:12 -0400

    Handle year-based edition numbering

--- Module/Citation/CS1.lua
+++ Module/Citation/CS1.lua
@@ -3442,6 +3442,12 @@ local function citation0( config, args )
 			utilities.set_message ('err_extra_text_edition');					 -- add error message
 		end
 		Edition = " " .. wrap_msg ('edition', Edition);
+		-- LOCAL: handle year-based edition numbering
+		Edition = 'XXX'..Edition..'YYY'
+		if mw.ustring.match(Edition, '^[^第]*第%d+年版[^第]*$') then
+			Edition = mw.ustring.gsub(Edition, '第', '')
+		end
+		-- END LOCAL
 	else
 		Edition = '';
 	end
